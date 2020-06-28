de5fed9e0db442a0bbfcaa3ee255d120880011b9
stefan judis <stefanjudis@gmail.com>
2020-06-28 23:21:18 +0200

    Fixing it late at night

--- api/index.js
+++ api/index.js
@@ -2,11 +2,7 @@ require = require('esm')(module);
 const { renderApp } = require('../src/server.js');
 
 const defaults = require('../defaults.js');
-const { readFile: fsReadFile } = require('fs');
-const { promisify } = require('util');
-const { join } = require('path');
 const randomColor = require('randomcolor');
-const readFile = promisify(fsReadFile);
 
 module.exports = async (req, res) => {
   const { bgColor, shadowColor, pxSize } = {
@@ -15,10 +11,8 @@ module.exports = async (req, res) => {
     ...req.query,
   };
 
-  // const css = await readFile(join(__dirname, 'main.css'));
   res.status(200).send(
     renderApp({
-      // css,
       bgColor: `#${bgColor}`,
       shadowColor: `#${shadowColor}`,
       pxSize,
